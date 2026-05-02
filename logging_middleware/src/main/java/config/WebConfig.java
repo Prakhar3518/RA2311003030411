035b04e7092ba47ca9726e78c322a7312e600ac8
Prakhar Chaudhary <prakharchaudhary0302@gmail.com>
2026-05-02 12:11:23 +0530

    interceptor register added

---
+++ logging_middleware/src/main/java/config/WebConfig.java
@@ -0,0 +1,27 @@
+package config;
+
+import com.middleware.logging.middleware.RequestLoggingMiddleware;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final RequestLoggingMiddleware requestLoggingMiddleware;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(requestLoggingMiddleware)
+                .addPathPatterns("/**");
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
