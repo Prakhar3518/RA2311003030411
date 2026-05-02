f2edd4d0811f58963f6f8a4fcf2f1be8f3512de4
Prakhar Chaudhary <prakharchaudhary0302@gmail.com>
2026-05-02 11:36:39 +0530

    added log request

--- logging_middleware/src/main/java/com/middleware/logging/model/LogRequest.java
+++ logging_middleware/src/main/java/com/middleware/logging/model/LogRequest.java
@@ -1,5 +1,25 @@
 package com.middleware.logging.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data  // to generate getters, setters
+@NoArgsConstructor  // Default constructor
+@AllArgsConstructor // Parameterized constructor
 public class LogRequest {
 
+    @JsonProperty("stack")
+    private String stack;
+
+    @JsonProperty("level")
+    private String level;
+
+    @JsonProperty("package")
+    private String packageName;
+
+    @JsonProperty("message")
+    private String message;
 }
+
